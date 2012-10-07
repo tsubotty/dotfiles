556e1f9f3bc1acd14ba9cb80dcb3e8ba440d63b5
Tsubone Seiya <karasuhaneto@gmail.com>
2012-10-07 17:10:56 +0900

    gitignore変更 setup.shの問題解決

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ DOT_FILES=( .zsh .zshrc .emacs.d .gemrc .gitconfig .vimrc .gvimrc .vim .vimperat
 
 for file in ${DOT_FILES[@]}
 do
-    ln -s $HOME/dotfiles/$file $HOME/$file
+    ln -sn $HOME/dotfiles/$file $HOME/$file
 done
 
 ln -s $HOME/dotfiles/private.xml /Users/tsubone/Library/Application\ Support/KeyRemap4MacBook/private.xml
