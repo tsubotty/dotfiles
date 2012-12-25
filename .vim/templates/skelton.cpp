24c9210917711bb05bfbea4374f2939e1d7926c5
Tsubone Seiya <karasuhaneto@gmail.com>
2012-12-26 08:41:00 +0900

    vimのテンプレート群追加

---
+++ .vim/templates/skelton.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <list>
+#include <deque>
+#include <stack>
+#include <queue>
+#include <bitset>
+#include <set>
+#include <map>
+
+int main()
+{
+    using namespace std;
+}
